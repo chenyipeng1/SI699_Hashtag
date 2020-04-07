689bd8e98f960f234fd503a9f172f8faad628e4b
Yipeng Chen <chenyp@gl-login2.arc-ts.umich.edu>
2020-04-07 17:07:15 -0400

    clean master + wh

--- model/text_encoder.py
+++ model/text_encoder.py
@@ -60,3 +60,50 @@ class EncoderRNN(nn.Module):
     def initHidden(self, batch_size):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         return torch.zeros(1, batch_size, self.hidden_size, device=device)
+
+
+
+class EncoderGRU(nn.Module):
+    def __init__(self, input_size, embed_size, hidden_size, num_output):
+    
+        # imput_size is the number of words in the class Lang defined in ../src/read_text.py. For example: input_lang.n_words
+
+        # sample usage:        
+        # hidden_size = 256
+        # encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+
+
+        super(EncoderGRU, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, embed_size)
+        self.gru = nn.GRU(embed_size, hidden_size, batch_first=True)
+
+        self.bn2 = nn.BatchNorm1d(hidden_size)
+        self.fc = nn.Linear(hidden_size, num_output)
+        
+
+
+    def forward(self, texts, text_lengths):
+        # input: texts (B, T), text_lengths (B)
+        # output: text_features (B, hidden_size) 
+        batch_size, _ = texts.shape
+        embedded = self.embedding(texts)
+        # embedded (B, T, text_vocab_size)
+        embedded_packed = pack_padded_sequence(embedded, text_lengths, batch_first=True, enforce_sorted=False)
+        output, _ = self.gru(embedded_packed, self.initHidden(batch_size))
+        output_padded = pad_packed_sequence(output, batch_first=True, padding_value=0)[0] # (B, T, hidden_size) 
+        mask = (text_lengths-1).unsqueeze(1).unsqueeze(2).expand(-1,-1,self.hidden_size).long()
+        # return features of last timestep
+        out=output_padded.gather(1, mask).squeeze()
+
+        out = self.bn2(out)
+        out = self.fc(out)
+
+        return out
+
+
+
+    def initHidden(self, batch_size):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        return torch.zeros(1, batch_size, self.hidden_size, device=device)
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
 source /scratch/si699w20_cbudak_class_root/si699w20_cbudak_class/shared_data/JI_team/venv/bin/activate
+
