7484d092809dd430adb8380501fa4decdff272a2
Yuying Li <yuyingli@gl-login2.arc-ts.umich.edu>
2020-04-10 20:17:41 -0400

    ignore index

--- model/focal_loss.py
+++ model/focal_loss.py
@@ -3,13 +3,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-class FocalLoss(nn.Module):
-    def __init__(self, gamma=0, alpha=None, size_average=True):
-        super(FocalLoss, self).__init__()
+class BinaryFocalLoss(nn.Module):
+    def __init__(self, gamma=0, ignore_index=None, alpha=None, size_average=True):
+        super(BinaryFocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
         if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
         if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        self.ignore_index = ignore_index
         self.size_average = size_average
 
     def forward(self, input, target):
@@ -17,7 +18,7 @@ class FocalLoss(nn.Module):
             input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-#        target = target.view(-1,1)
+
         target = target.contiguous().view(-1,1)
 
         logpt = F.log_softmax(input,dim=1)
--- model/train.py
+++ model/train.py
@@ -6,7 +6,7 @@ from dataloader import TweetData
 from torch.nn.utils.rnn import pack_padded_sequence
 import time
 from PIL import ImageFile
-from focal_loss import FocalLoss
+from focal_loss import BinaryFocalLoss
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def count_corrects(label, predict, label_lengths):
@@ -20,14 +20,14 @@ def train():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using ", device)
-    tweet_data = TweetData(batch_size=4, file_size=100)
+    tweet_data = TweetData(batch_size=4, file_size=50000)
     text_vocab_size = tweet_data.label_generator.text_vocab.n_words
     label_vocab_size = tweet_data.label_generator.label_num
 
     cnn_rnn = CNN_RNN(text_vocab_size=text_vocab_size, text_embed_size=128, text_hidden_size = 512, \
             label_vocab_size=label_vocab_size, label_hidden_size = 256, resnet_version="resnet18", train_resnet=False)
     
-    USE_FOCAL_LOSS = False
+    USE_FOCAL_LOSS = True
 
     cnn_rnn = cnn_rnn.to(device)
     epoch_num = 20
@@ -36,7 +36,7 @@ def train():
     criterion = None
     if USE_FOCAL_LOSS:
         print("Using Focal Loss")
-        criterion = FocalLoss(gamma=2)
+        criterion = BinaryFocalLoss(gamma=2,ignore_index=0)
     else:
         print("Using Cross Entropy")   
         criterion = nn.CrossEntropyLoss(ignore_index=0)
