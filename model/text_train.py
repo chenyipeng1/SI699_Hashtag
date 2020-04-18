05163330528410a953b59c0db6282d189464823a
Fei Yi <feiyi@gl-login1.arc-ts.umich.edu>
2020-04-18 17:17:19 -0400

    fix label bug

--- model/text_train.py
+++ model/text_train.py
@@ -18,13 +18,14 @@ def train():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using ", device)
-    tweet_data = TweetData(batch_size=4, file_size=100)
+    tweet_data = TweetData(batch_size=8, file_size=100)
     text_vocab_size = tweet_data.label_generator.text_vocab.n_words
     label_vocab_size = tweet_data.label_generator.label_num
-    text_rnn=EncoderGRU(input_size=text_vocab_size, embed_size=512, hidden_size=512,num_output=label_vocab_size).to(device)
+    text_rnn=EncoderGRU(input_size=text_vocab_size, embed_size=128, hidden_size=128,num_output=label_vocab_size).to(device)
     epoch_num = 20
-    optimizer = torch.optim.Adam(text_rnn.parameters(), lr=0.01)
+    optimizer = torch.optim.Adam(text_rnn.parameters(), lr=0.001)
     criterion = nn.CrossEntropyLoss(ignore_index=0)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
     for epoch in range(epoch_num):
         since = time.time()
@@ -32,13 +33,13 @@ def train():
         print('-' * 10)
         running_loss = 0.0
         running_corrects = 0
-        running_size = 0
+        running_size = 0.0
 
         for phase in ["train", "val", "test"]:
 
             for batch_data in tweet_data.dataloaders[phase]:
-                print(batch_data)
-                break
+                #print(batch_data)
+                #break
                 text = batch_data["text"].to(device)
                 # image = batch_data["image"].to(device)
                 label = batch_data["label"].to(device)
@@ -53,21 +54,22 @@ def train():
 
                 outputs=text_rnn.forward(text,text_lengths)
                 _, predicts = torch.max(outputs, 1) # B, T
-                label_trim = label[:,1]
+                label_trim = label[:,0]
 
 
                 loss = criterion(outputs, label_trim)
                 loss.backward()
                 optimizer.step()
                 
-                running_loss += loss.item() * torch.sum(label_lengths-1)
+                running_loss += loss.item() * label_trim.shape[0]
                 running_corrects += torch.sum(label_trim == predicts)
-                running_size += torch.sum(label_lengths-1)
+                running_size += label_trim.shape[0]
 
             epoch_loss = running_loss / running_size
             epoch_acc = running_corrects / running_size
 
             time_elapsed = time.time() - since
             print('{} Loss: {:.4f} Acc: {:.4f} in {:.0f}m {:.0f}s'.format(phase, epoch_loss, epoch_acc, time_elapsed//60, time_elapsed%60))
+        scheduler.step()
 
 train()
