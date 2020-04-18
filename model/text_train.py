784891d1b50c48337b14b8ac9cceed5ef1943123
Fei Yi <feiyi@Feis-MacBook-Pro.local>
2020-04-18 16:24:40 -0400

    add test

--- model/text_train.py
+++ model/text_train.py
@@ -18,7 +18,7 @@ def train():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using ", device)
-    tweet_data = TweetData(batch_size=4, file_size=1000)
+    tweet_data = TweetData(batch_size=4, file_size=100)
     text_vocab_size = tweet_data.label_generator.text_vocab.n_words
     label_vocab_size = tweet_data.label_generator.label_num
     text_rnn=EncoderGRU(input_size=text_vocab_size, embed_size=512, hidden_size=512,num_output=label_vocab_size).to(device)
@@ -33,38 +33,40 @@ def train():
         running_loss = 0.0
         running_corrects = 0
         running_size = 0
-        
-        for batch_data in tweet_data.dataloaders["train"]:
-            batch_data = batch_data
-            text = batch_data["text"].to(device)
-            # image = batch_data["image"].to(device)
-            label = batch_data["label"].to(device)
-            label_lengths = batch_data["label_length"].to(device)
-            text_lengths = batch_data["text_length"].to(device)
-            optimizer.zero_grad()
 
-            # outputs = cnn_rnn.forward(text, image, label, text_lengths) # B, T, label_vocab_size
-            # _, predicts = torch.max(outputs, 2) # B, T
-            # label_trim = label[:,1:]
+        for phase in ["train", "val", "test"]:
 
+            for batch_data in tweet_data.dataloaders[phase]:
+                batch_data = batch_data
+                text = batch_data["text"].to(device)
+                # image = batch_data["image"].to(device)
+                label = batch_data["label"].to(device)
+                label_lengths = batch_data["label_length"].to(device)
+                text_lengths = batch_data["text_length"].to(device)
+                optimizer.zero_grad()
 
-            outputs=text_rnn.forward(text,text_lengths)
-            _, predicts = torch.max(outputs, 1) # B, T
-            label_trim = label[:,1]
+                # outputs = cnn_rnn.forward(text, image, label, text_lengths) # B, T, label_vocab_size
+                # _, predicts = torch.max(outputs, 2) # B, T
+                # label_trim = label[:,1:]
 
 
-            loss = criterion(outputs, label_trim)
-            loss.backward()
-            optimizer.step()
-            
-            running_loss += loss.item() * torch.sum(label_lengths-1)
-            running_corrects += torch.sum(label_trim == predicts)
-            running_size += torch.sum(label_lengths-1)
+                outputs=text_rnn.forward(text,text_lengths)
+                _, predicts = torch.max(outputs, 1) # B, T
+                label_trim = label[:,1]
 
-        epoch_loss = running_loss / running_size
-        epoch_acc = running_corrects / running_size
-        phase = "train"
-        time_elapsed = time.time() - since
-        print('{} Loss: {:.4f} Acc: {:.4f} in {:.0f}m {:.0f}s'.format(phase, epoch_loss, epoch_acc, time_elapsed//60, time_elapsed%60))
+
+                loss = criterion(outputs, label_trim)
+                loss.backward()
+                optimizer.step()
+                
+                running_loss += loss.item() * torch.sum(label_lengths-1)
+                running_corrects += torch.sum(label_trim == predicts)
+                running_size += torch.sum(label_lengths-1)
+
+            epoch_loss = running_loss / running_size
+            epoch_acc = running_corrects / running_size
+
+            time_elapsed = time.time() - since
+            print('{} Loss: {:.4f} Acc: {:.4f} in {:.0f}m {:.0f}s'.format(phase, epoch_loss, epoch_acc, time_elapsed//60, time_elapsed%60))
 
 train()
