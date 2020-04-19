637d50fd63d28d6700ab540d1c3613214c0c8621
Fei Yi <feiyi@gl-login2.arc-ts.umich.edu>
2020-04-19 11:03:16 -0400

    modify myjob.sh

--- myjob.sh
+++ myjob.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1g
-#SBATCH --time=10:00:00
+#SBATCH --time=40:00:00
 #SBATCH --account=si699w20_cbudak_class
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
