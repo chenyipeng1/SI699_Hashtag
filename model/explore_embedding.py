86afa80a256267863d995abb160ac0ed1f784892
Fei Yi <feiyi@Feis-MacBook-Pro.local>
2020-04-26 21:20:47 -0400

    test label embedding exploration

---
+++ model/explore_embedding.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from cnn_rnn import CNN_RNN
+from dataloader import TweetData
+from torch.nn.utils.rnn import pack_padded_sequence
+import time
+from PIL import ImageFile
+from focal_loss import FocalLoss
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+def explore():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using ", device)
+    tweet_data = TweetData(batch_size=4, file_size=100)
+    for batch_data in tweet_data.dataloaders["train"]:
+        print("Seed fixed")
+        print("For whole 847-data with batch_size 8")
+        print("Below should be 11., 12., 12.,  8.,  6.,  7.,  9.,  7.")
+        print(batch_data["text_length"])
+        break
+    label2tag = tweet_data.label_generator.label2tag
+    text_vocab_size = tweet_data.label_generator.text_vocab.n_words
+    label_vocab_size = tweet_data.label_generator.label_num
+
+    cnn_rnn = CNN_RNN(text_vocab_size=text_vocab_size, text_embed_size=128, text_hidden_size = 128, \
+            label_vocab_size=label_vocab_size, label_hidden_size = 128, resnet_version="resnet18", train_resnet=False)
+    
+    MODEL_PATH = "/home/feiyi/SI699_Hashtag/serialized/cross.pt"
+    cnn_rnn.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device("cpu")))
+
+    print(cnn_rnn.decoder.embed.weight.shape)
